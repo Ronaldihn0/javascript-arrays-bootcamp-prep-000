0f8a01c1111413a49e45009e4f72d438506bc309
Ronald DeBeras <rdeberas@gmail.com>
2018-01-10 20:59:55 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -36,6 +36,6 @@ function destructivelyRemoveElementFromBeginningOfArray(array, element) {
   array.shift(element);
   return array
 }
-function removeElementFromBeginningOfArray(array) {
- array.slice(0)
-}
+function destructivelyRemoveElementFromEndOfArray(array, element) {
+  array.pop()
+  return array
